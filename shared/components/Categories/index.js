00ec9eff13f23a6278436b1a2430c5a2cb38fcb0
fergmac <f.macconnell@gmail.com>
2016-09-28 14:03:21 -0700

    add site icon

--- shared/components/Categories/index.js
+++ shared/components/Categories/index.js
@@ -4,20 +4,19 @@ import AppBar from 'material-ui/AppBar';
 import IconButton from 'material-ui/IconButton';
 import Drawer from 'material-ui/Drawer';
 import MenuItem from 'material-ui/MenuItem';
-import CommunicationImportContacts from 'material-ui/svg-icons/communication/import_contacts';
-import FontIcon from 'material-ui/FontIcon';
+import SiteIcon from 'material-ui/svg-icons/communication/import-contacts';
+// import FontIcon from 'material-ui/FontIcon';
 // import Data from '../../data.js';
 
 const Categories = () => (
   <Drawer>
-    <FontIcon className="material-icons" style={iconStyles}>home</FontIcon>
     <AppBar
       className={style.categories}
       title={<span>REDit</span>}
       iconElementLeft={
         <IconButton>
-          <CommunicationImportContacts style={iconStyles} color={white500} />
-        </IconButton> 
+          <SiteIcon />
+        </IconButton>
      }
     />
     <MenuItem>React</MenuItem>
