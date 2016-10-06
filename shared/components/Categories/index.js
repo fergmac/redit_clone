7235ab71843391d4353e24eac60e6c5c36894104
fergmac <f.macconnell@gmail.com>
2016-10-06 11:25:33 -0700

    change categories from stateless function into class as a container component

--- shared/components/Categories/index.js
+++ shared/components/Categories/index.js
@@ -1,26 +1,49 @@
-import React from 'react';
+import React, { PureComponent } from 'react';
 import style from './style.css';
 import AppBar from 'material-ui/AppBar';
 import IconButton from 'material-ui/IconButton';
 import Drawer from 'material-ui/Drawer';
 import Week from '../Week';
 import SiteIcon from 'material-ui/svg-icons/communication/import-contacts';
-import data from '../../data.js';
+// import data from '../../data.js';
+import { connect } from 'react-redux';
 
+class Categories extends PureComponent {
+  render() {
+    return (
+      <Drawer>
+        <AppBar
+          className={style.categories}
+          title={<span>REDit</span>}
+          iconElementLeft={
+            <IconButton>
+              <SiteIcon />
+            </IconButton>
+            }
+        />
+          {this.props.weeks.map((week, index) => (<Week week={week} key={index} />))}
+      </Drawer>
+    );
+  }
+}
 
-const Categories = () => (
-  <Drawer>
-    <AppBar
-      className={style.categories}
-      title={<span>REDit</span>}
-      iconElementLeft={
-        <IconButton>
-          <SiteIcon />
-        </IconButton>
-     }
-    />
-    {data.weeks.map((week, index) => (<Week week={week} key={index} />))}
-  </Drawer>
-);
+const mapStateToProps = state => ({
+  weeks: state.weeks,
+});
 
-export default Categories;
+export default connect(mapStateToProps)(Categories);
+
+// const Categories = () => (
+//   <Drawer>
+//     <AppBar
+//       className={style.categories}
+//       title={<span>REDit</span>}
+//       iconElementLeft={
+//         <IconButton>
+//           <SiteIcon />
+//         </IconButton>
+//      }
+//     />
+//     {data.weeks.map((week, index) => (<Week week={week} key={index} />))}
+//   </Drawer>
+// );
