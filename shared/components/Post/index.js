ceb67b2577eaffb4fcbf5ba61f764d01865dae15
fergmac <f.macconnell@gmail.com>
2016-10-06 15:12:31 -0700

    add votes FlatButton to PostList

--- shared/components/Post/index.js
+++ shared/components/Post/index.js
@@ -1,19 +1,15 @@
 import React, { PropTypes } from 'react';
-import styles from './style.css';
+// import styles from './style.css';
 // import Chip from 'material-ui/Chip';
-import Paper from 'material-ui/Paper';
+// import Paper from 'material-ui/Paper';
 
 const Post = ({ post }) => (
-  <Paper className={styles.postContainer}>
-    <div>
-      <a href=""><h3>{post.title}</h3></a>
-      <p>{post.description}</p>
-      <p>{post.votes}</p>
-    </div>
-    <div>
+  <div>
+    <a href=""><h3>{post.title}</h3></a>
+    <p>{post.description}</p>
+      {/* <button>{post.votes}</button> */}
    {/* {post.lessons.map((lesson, index) => (<Chip key={index}>{lesson}</Chip>))} */}
-    </div>
-  </Paper>
+  </div>
 
 );
 
--- shared/components/PostList/index.js
+++ shared/components/PostList/index.js
@@ -1,17 +1,27 @@
 import React, { PureComponent } from 'react';
 import styles from './style.css';
-// import data from '../../data.js';
+import Paper from 'material-ui/Paper';
 import Post from '../Post';
 import { connect } from 'react-redux';
+import { voteUp, sortByPopularity } from '../../redux/modules/posts.js';
+import { bindActionCreators } from 'redux';
+import FlatButton from 'material-ui/FlatButton';
 
 
 class PostList extends PureComponent {
   render() {
+    const truth = true;
     const { posts, params } = this.props;
     const correctPost = posts.filter((post) => parseInt(params.lessonId, 10) === post.id);
     return (
       <div className={styles.postListContainer}>
-         {correctPost.map((post) => (<Post post={post} key={post.id} />))}
+         {correctPost.map((post) => (
+           <Paper key={post.id} className={styles.postContainer} >
+             <Post post={post} />
+             <FlatButton label="Votes" primary={truth} onClick={this.props.voteUp}>
+              {post.votes}
+             </FlatButton>
+           </Paper>))}
       </div>
     );
   }
@@ -21,7 +31,15 @@ const mapStateToProps = state => ({
   posts: state.posts,
 });
 
-const mapDispatchToProps = { };
+// const mapDispatchToProps = dispatch => ({
+//    dispatch(voteUp)
+// });
+
+function mapDispatchToProps(dispatch) {
+  return {
+    actions: bindActionCreators(Object.assign({}, voteUp, sortByPopularity), dispatch),
+  };
+}
 
 export default connect(mapStateToProps, mapDispatchToProps)(PostList);
 
