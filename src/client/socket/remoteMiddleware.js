b31746ec511d29d761ef5827b90656d81924664d
fergmac <f.macconnell@gmail.com>
2016-10-12 14:14:49 -0700

    add function so that only actions with meta remote true are sent to server

--- src/client/socket/remoteMiddleware.js
+++ src/client/socket/remoteMiddleware.js
@@ -1,6 +1,9 @@
 const remoteMiddleware = socket => store => next => action => {
+  if (action.meta && action.meta.remote) {
+    delete action.meta;
+    socket.emit('state', action);
+  }
   console.log('remote', store.getState(), action);
-  socket.emit('state', action);
   console.log('remote', action);
   return next(action);
 };
