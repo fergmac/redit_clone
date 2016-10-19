112f091f8c52d9dce2f5334c6ffb2195e2e11be2
fergmac <f.macconnell@gmail.com>
2016-10-19 16:12:28 -0700

    add redux-form to LoginForm page

--- src/client/components/LoginForm/index.js
+++ src/client/components/LoginForm/index.js
@@ -1,10 +1,45 @@
-import React from 'react';
+import React, { PureComponent } from 'react';
+import { Field, reduxForm } from 'redux-form';
 import styles from './style.css';
 
-const LoginForm = () => (
-  <div className={styles.loginFormPage}>
-    <h1>Login Form</h1>
-  </div>
-);
+class LoginForm extends PureComponent {
+  render() {
+    const { handleSubmit } = this.props;
+    console.log(this.props);
+    return (
+      <div className={styles.loginFormPage}>
+        <h1>LoginForm</h1>
+        <form onSubmit={handleSubmit((fields) => console.log(fields))}>
+          <Field
+            name="email"
+            component="input"
+            type="text"
+          />
+          <Field
+            name="password"
+            component="input"
+            type="password"
+          />
+          <button type="submit">Submit</button>
+        </form>
 
-export default LoginForm;
+      </div>
+    );
+  }
+}
+
+export default reduxForm({
+  form: 'login',
+})(LoginForm);
+
+
+// import React from 'react';
+// import styles from './style.css';
+
+// const LoginForm = () => (
+//   <div className={styles.loginFormPage}>
+//     <h1>Login Form</h1>
+//   </div>
+// );
+
+// export default LoginForm;
