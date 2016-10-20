2a91768fe05b9b761a88328f1f095cd3bb37d28e
fergmac <f.macconnell@gmail.com>
2016-10-20 13:12:15 -0700

    change AddAlbum to Class Component, connect to store and dispatch saveAlbum(newAlbum)

--- src/client/components/AddAlbum/index.js
+++ src/client/components/AddAlbum/index.js
@@ -1,10 +1,43 @@
-import React from 'react';
+import React, { PureComponent } from 'react';
 import styles from './style.css';
+import { connect } from 'react-redux';
+import { saveAlbum } from '../../../shared/redux/modules/albums';
 // was create post
-const addAlbum = () => (
-  <div className={styles.addAlbum}>
-    <h1>Add Album Page</h1>
-  </div>
-);
 
-export default addAlbum;
+class AddAlbum extends PureComponent {
+
+  componentDidMount() {
+    this.props.saveAlbum({
+      title: 'ferg',
+      description: 'fingerstyle guitar',
+      link: 'https://www.youtube.com/watch?v=ygDs_pkEHDs',
+    });
+  }
+  render() {
+    return (
+      <div className={styles.addAlbum}>
+        <h1>Add Album Page</h1>
+      </div>
+      );
+  }
+}
+
+const mapStateToProps = state => ({
+  albums: state.albums,
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  saveAlbum: (newAlbum) => {
+    dispatch(saveAlbum(newAlbum));
+  },
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(AddAlbum);
+
+// const addAlbum = () => (
+//   <div className={styles.addAlbum}>
+//     <h1>Add Album Page</h1>
+//   </div>
+// );
+
+// export default addAlbum;
