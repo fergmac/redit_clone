2876d4f9a8aaddbf2225892ac8f81d9172113c59
fergmac <f.macconnell@gmail.com>
2016-10-23 23:15:33 -0700

    add genres, users and artists to combined reducers

--- src/shared/redux/reducers.js
+++ src/shared/redux/reducers.js
@@ -1,8 +1,12 @@
 const combineReducers = require('redux').combineReducers;
 const genres = require('./modules/genres.js').default;
 const albums = require('./modules/albums.js').default;
+const users = require('./modules/users.js').default;
+const artists = require('./modules/artists.js').default;
 
 module.exports = combineReducers({
-  genres, // weeks
+  genres, // genres
   albums, // posts
+  users,
+  artists,
 });
