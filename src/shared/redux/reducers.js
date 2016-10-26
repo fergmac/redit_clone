2ec41ee98157617bff581fc202f044b07fbd5701
fergmac <f.macconnell@gmail.com>
2016-10-26 10:38:26 -0700

    add routerReducer and redux form to shared reducer

--- src/shared/redux/reducers.js
+++ src/shared/redux/reducers.js
@@ -3,10 +3,14 @@ const genres = require('./modules/genres.js').default;
 const albums = require('./modules/albums.js').default;
 const users = require('./modules/users.js').default;
 const artists = require('./modules/artists.js').default;
+const routerReducer = require('react-router-redux').routerReducer;
+const form = require('redux-form').reducer;
 
 module.exports = combineReducers({
   genres, // genres
   albums, // posts
   users,
   artists,
+  routing: routerReducer,
+  form,
 });
